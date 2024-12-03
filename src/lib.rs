2a3d92340f8005642db2e0c826ae57d1cda09601
gaburoff <gabrielcha@uroff.net>
2024-12-03 16:41:52 -0300

    ted to pub

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,4 @@ mod enums;
 pub mod key;
 mod models;
 pub mod sign;
-mod ted;
 pub mod utils;
--- src/sign/mod.rs
+++ src/sign/mod.rs
@@ -1,7 +1,10 @@
 use handlers::get_signature;
+use openssl::{hash::MessageDigest, pkey::PKey, rsa::Rsa, sign::Signer};
 
 use crate::{
-    enums::SignatureType, key::KeyPairCert, utils::insert_element::insert_after_element
+    enums::SignatureType,
+    key::KeyPairCert,
+    utils::{encode::encode, insert_element::insert_after_element, latin::to_latin1},
 };
 
 mod handlers;
@@ -19,4 +22,20 @@ pub fn sign_envio(xml_str: &str, key_pair_cert: KeyPairCert) -> Result<String, &
 pub fn sign_token(xml_str: &str, key_pair_cert: KeyPairCert) -> Result<String, &'static str> {
     let signature = get_signature(xml_str, SignatureType::Token, key_pair_cert)?;
     Ok(insert_after_element(xml_str, &signature, "item"))
+}
+
+pub fn sign_ted(rsask: &str, dd: &str) -> Result<String, &'static str> {
+    let dd_one_line = dd.replace('\n', "");
+    let rsa = Rsa::private_key_from_pem(rsask.as_bytes()).map_err(|_| "Failed to parse RSA key")?;
+    let keypair = PKey::from_rsa(rsa).map_err(|_| "Failed to create key pair")?;
+
+    // Sign the data
+    let mut signer =
+        Signer::new(MessageDigest::sha1(), &keypair).map_err(|_| "Failed to create signer")?;
+    let latin1_vec = to_latin1(&dd_one_line);
+    signer
+        .update(&latin1_vec)
+        .map_err(|_| "Failed to update signer")?;
+    let signature = signer.sign_to_vec().map_err(|_| "Failed to sign data")?;
+    Ok(encode(signature))
 }
